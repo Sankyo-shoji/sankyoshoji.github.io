c979cb801bf29dddcd9dd31b4ae9d47dbc2b587d
Sankyo-shoji <114405555+Sankyo-shoji@users.noreply.github.com>
2022-10-01 17:23:44 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1657520569-Q9mAmqZV";
+    var liffId = "1657505143-oqx9ae67";
     initializeLiff(liffId);
 })
 
